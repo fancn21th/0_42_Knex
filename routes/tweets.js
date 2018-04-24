0522165f2bd3042ec84649d7ab833fbf5ba7b35d
alex.fan <alex.fan@okchem.com>
2018-04-24 17:28:55 +0800

    403

--- routes/tweets.js
+++ routes/tweets.js
@@ -2,23 +2,30 @@ const router = require('express').Router()
 const db = require('../db')
 
 function loginRequired(req, res, next) {
-    if(!req.isAuthenticated()){
-        return res.redirect('/login')
-    }
-    next()
+  if (!req.isAuthenticated()) {
+    return res.redirect('/login')
+  }
+  next()
+}
+
+function adminRequired(req, res, next) {
+  if (!req.user.is_admin) {
+    return res.render('403')
+  }
+  next()
 }
 
 router
   .get('/tweets', loginRequired, (req, res, next) => {
-      db('tweets')
-        .where('user_id', req.user.id)
-        .then(tweets => {
-            res.render('tweets', {
-              tweets
-            })
-        }, next)
+    db('tweets')
+      .where('user_id', req.user.id)
+      .then(tweets => {
+        res.render('tweets', {
+          tweets
+        })
+      }, next)
   })
-  .get('/allTweets', loginRequired, (req, res, next) => {
+  .get('/allTweets', loginRequired, adminRequired, (req, res, next) => {
     db('tweets')
       .then(tweets => {
         res.render('tweets', {
